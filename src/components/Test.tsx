7ca4a6fd8fe006da9f052eb9ef3c80841ddc0590
Eric Marques <emarques3@gmail.com>
2021-01-17 12:12:20 -0600

    Improving HOC usage

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { useSplit } from 'split-react';
 
 export const Test = ({ splitName }: { splitName: string }) => {
   const split = useSplit(splitName, false);
   const color = split ? '#00FF00' : '#FF0000'
   return (
-    <h1>The flag <i>{splitName}</i> is <strong style={{ color }}>{split ? 'ON' : 'OFF'}</strong></h1>
+    <h1>Hook: The flag <i>{splitName}</i> is <strong style={{ color }}>{split ? 'ON' : 'OFF'}</strong></h1>
   );
 };
--- src/components/TestHOC.tsx
+++ src/components/TestHOC.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import { withSplit } from 'split-react';
+
+const FLAG = 'test1';
 
 const TestHOC = ({ splitFlagOn }: { splitFlagOn?: boolean }) => {
   const color = splitFlagOn ? '#00FF00' : '#FF0000';
 
   return (
-    <h1>The flag  is <strong style={{ color }}>{splitFlagOn ? 'ON' : 'OFF'}</strong></h1>
+    <h1>HOC: The flag <i>{FLAG}</i> is <strong style={{ color }}>{splitFlagOn ? 'ON' : 'OFF'}</strong></h1>
   );
 };
 
-export default withSplit(TestHOC, 'test1', true);
+export default withSplit(TestHOC, FLAG, true);
